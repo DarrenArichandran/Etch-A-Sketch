78700b8b5853f3ef6994b796cc4f920fae372fce
Darren Arichandran <d.arichandran@gmail.com>
2023-03-12 14:52:19 +0000

    Load v3 with functioning random rgb on hover, need to combine with opacity so that on 10 passes the square is completley black

--- script.js
+++ script.js
@@ -6,7 +6,13 @@ const container = document.getElementById('container');
 const docFrag = document.createDocumentFragment();
 let userInput = prompt("How many squares in your grid?");
 let gridNumber = [];
-
+function rainbow() {
+    let color = [];
+    for (let i = 0; i < 3; i++) {
+      color.push(Math.floor(Math.random() * 256));
+    }
+    return 'rgb(' + color.join(', ') + ')';
+  } 
 
     if (userInput != null && userInput <= 100) {
 
@@ -18,11 +24,14 @@ let gridNumber = [];
                 div.setAttribute('id','grid');
                 div.style.width = `calc(500px/${userInput})`;
                 div.style.height = `calc(500px/${userInput})`;
-
-                docFrag.appendChild(div);
+                div.addEventListener("mouseover", event => {
+                    event.target.style.backgroundColor = rainbow();
+                })
+                docFrag.appendChild(div)
             })
                 container.replaceChildren(docFrag);
     } else {
         alert('Try again')
     }
 });
+
